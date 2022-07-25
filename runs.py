02d386afadb95ce9bed9a93afa64217c6c6cb1a7
anil <anilsai029@gmail.com>
2022-07-25 11:52:02 +0530

    runs.py file updated

--- runs.py
+++ runs.py
@@ -1,10 +1,10 @@
 import os
-from re import L
+import numpy as np
  
-p1s = range(0,3)
-p2s = range(0,3)
+alphas = np.linspace(0.1,1.0,5)
+l1_ratiios = np.linspace(0.1,1.0,5)
 
-for p1 in p1s:
-    for p2 in p2s:
-        print(f"loggign experiment for p1={p1} and p2={p2}")
-        os.system(f"python demo.py -p1={p1} -p2={p2}")
+for p1 in alphas:
+    for p2 in l1_ratiios:
+        print(f"loggign experiment for alpha={p1} and l1_ratio={p2}")
+        os.system(f"python demo_ml.py -a={p1} -l1={p2}")
